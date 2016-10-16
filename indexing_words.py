a3a3609d5f565c1c6d576ca507d4b34cf6b1bcd8
Andy Chen <asjchen15@gmail.com>
2016-10-15 17:23:27 -0700

    Modified indexing.py

--- indexing_words.py
+++ indexing_words.py
@@ -3,18 +3,29 @@
 # such that for each word vi, the first letter lies in w1, the second in w2, etc
 
 import sys
+import itertools
+
+allCombos = False
 
 f = open('/usr/share/dict/words', 'r')
 word_dict = [word.lower() for word in f.read().split()]
 word_list = [word.lower() for word in sys.stdin.read().split()]
 solns = []
-for word in word_dict:
-	if len(word) == len(word_list):
-		is_valid = True
-		for i in range(len(word)):
-			is_valid = is_valid and (word[i] in list(word_list[i]))
-		if is_valid:
-			solns.append(word)
+
+if allCombos:
+	orders = itertools.permutations(word_list)
+else:
+	orders = [word_list]
+
+for order in orders:
+	for word in word_dict:
+		if len(word) == len(order):
+			is_valid = True
+			for i in range(len(word)):
+				is_valid = is_valid and (word[i] in list(order[i]))
+			if is_valid:
+				solns.append(word)
+				print word
 print solns
 
 
