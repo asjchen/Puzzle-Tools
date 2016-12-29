36654148497327c005bbc4ae330e4bfba7981c38
nphirning <nphirning@gmail.com>
2016-12-28 20:01:49 -0800

    word search solver complete

--- word_search_solver.py
+++ word_search_solver.py
@@ -1,16 +1,28 @@
 import sys
 import fileinput
+ 
+MIN_WORD_LENGTH = 4
 
-MIN_WORD_LENGTH = 3
-HORIZONTAL_OK = True
-VERTICAL_OK = True
-DIAGONAL_OK = True
-BACKWARDS_OK = False
-UPWARDS_OK = True
+# HORIZONTAL DIRECTIONS
 
-f = open('/usr/share/dict/words', 'r')
-word_dict = [word.lower() for word in f.read().split()]
-ws_list = [line.lower().strip() for line in fileinput.input()]
+HORIZONTAL_FORWARDS_OK = True
+HORIZONTAL_BACKWARDS_OK = True
+
+# VERTICAL DIRECTIONS
+
+VERTICAL_DOWN_OK = True
+VERTICAL_UP_OK = True
+
+# DIAGONAL DIRECTIONS
+
+DIAGONAL_DR_OK = True # diagonal down-right
+DIAGONAL_UR_OK = True # diagonal up-right
+DIAGONAL_DL_OK = True # diagonal down-left
+DIAGONAL_UL_OK = True # diagonal up-left
+
+
+
+################# TRIE FUNCTIONS #################
 
 def make_trie(words):
     root = dict()
@@ -34,41 +46,130 @@ def in_trie(trie, word):
         else:
             return False
 
-wst = make_trie(word_dict)
+########################################################
 
-def find_horizontal_words(backword_words, word_arr):
+
+
+def find_horizontal_words(valid_directions, word_arr, wst):
     final_words = set()
-    for line in word_arr:
+    for row in range(len(word_arr)):
+        line = word_arr[row]
         for i in range(len(line)):
 
             # Backward words
-            if backword_words:
-                for j in range(i):
-                    word = line[j:i + 1]
+            if valid_directions[1]:
+                for j in range(i + 1):
+                    word = line[j:i + 1][::-1]
                     if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
-                        final_words.add(word)
+                        final_words.add((word, (row, i)))
 
             # Forward words
-            for j in range(i + 1, len(line)):
-                word = line[i: j + 1]
-                if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
-                    final_words.add(word)
+            if valid_directions[0]:
+                for j in range(i + 1, len(line)):
+                    word = line[i: j + 1]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, i)))
     return final_words
 
-print find_horizontal_words(BACKWARDS_OK, ws_list)
+def find_vertical_words(valid_directions, word_arr, wst):
+    final_words = set()
+    for row in range(len(word_arr)):
+        rows_below = len(word_arr) - row - 1
+        for col in range(len(word_arr[0])):
 
-def find_vertical_words(upward_words, word_arr):
-    vert_array = []
-    for j in range(len(word_arr[0])):
-        string = ""
-        for i in range(len(word_arr)):
-            string += ws_list[i][j]
-        vert_array.append(string)
-    return find_horizontal_words(upward_words, vert_array)
+            # Vertical Down
+            if valid_directions[0]:
+                word = ''
+                for i in range(rows_below + 1):
+                    word += word_arr[row + i][col]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, col)))
 
-print find_vertical_words(UPWARDS_OK, ws_list)
+            # Vertical Up
+            if valid_directions[1]:
+                word = ''
+                for i in range(row + 1):
+                    word += word_arr[row - i][col]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, col)))
+    return final_words
 
 
+def find_diagonal_words(valid_directions, word_arr, wst):
+    final_words = set()
+    for row in range(len(word_arr)):
+        rows_below = len(word_arr) - row - 1
+        for col in range(len(word_arr[0])):
+            columns_right = len(word_arr[0]) - col - 1
+
+            # Diagonal Down-Right
+            if valid_directions[0]:
+                word = ''
+                for i in range(min(columns_right, rows_below) + 1):
+                    word += word_arr[row + i][col + i]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, col)))
+
+            # Diagonal Down-Left
+            if valid_directions[1]:
+                word = ''
+                for i in range(min(col, rows_below) + 1):
+                    word += word_arr[row + i][col - i]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, col)))
+
+            # Diagonal Up-Left
+            if valid_directions[2]:
+                word = ''
+                for i in range(min(col, row) + 1):
+                    word += word_arr[row - i][col - i]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, col)))
+
+            # Diagonal Up-Right
+            if valid_directions[3]:
+                word = ''
+                for i in range(min(columns_right, row) + 1):
+                    word += word_arr[row - i][col + i]
+                    if len(word) >= MIN_WORD_LENGTH and in_trie(wst, word):
+                        final_words.add((word, (row, col)))
+
+    return final_words
+
+
+def main():
+    f = open('/usr/share/dict/words', 'r')
+    word_dict = [word.lower() for word in f.read().split()]
+    wst = make_trie(word_dict)
+
+    print "Input each line of the word search on a separate line"
+    print "Press CTRL+D on a new line to begin searching"
+
+    ws_list = [line.lower().strip() for line in fileinput.input()] 
+    print '\n', "Processing..."
+
+    valid_horizontal_directions = [HORIZONTAL_FORWARDS_OK, HORIZONTAL_BACKWARDS_OK]
+    horizontal_words = find_horizontal_words(valid_horizontal_directions, ws_list, wst)
+
+    valid_vertical_directions = [VERTICAL_DOWN_OK, VERTICAL_UP_OK]
+    vertical_words = find_vertical_words(valid_vertical_directions, ws_list, wst)
+
+    valid_diagonal_directions = [DIAGONAL_DR_OK, DIAGONAL_DL_OK, DIAGONAL_UL_OK, DIAGONAL_UR_OK]
+    diagonal_words = find_diagonal_words(valid_diagonal_directions, ws_list, wst)
+
+    words = list(horizontal_words) + list(vertical_words) + list(diagonal_words)
+    words = sorted(words)
+
+    print "Processed!"
+
+    print "Word", '\t\t', "Row", '\t', "Column"
+    print "---------------------------------"
+    for (word, (row, col)) in words:
+        print word, '\t\t', row, '\t', col
+
+
+if __name__ == '__main__':
+    main()
 
 
 
